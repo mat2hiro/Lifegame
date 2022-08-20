bb2262de35a2b0633fb154fb3d9ebc4e42f25988
matsume-hiro <matsume@techflyer.co.jp>
2022-08-21 03:22:17 +0900

    spa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   target: "static",
   css: ["@/assets/css/main.scss", "@/assets/css/tailwind.css"],
   plugins: ["@/plugins/fontawesome.ts"],
