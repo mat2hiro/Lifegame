1407a41ddef996c89e578cf78fb8619fa0aa258d
matsume-hiro <matsume@techflyer.co.jp>
2022-08-21 01:31:21 +0900

    add actions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  head: {
+    title: "LifeGame",
+  },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/main.scss"],
   plugins: ["@/plugins/fontawesome.ts"],
