67c6af512b3853d1c05fa30d0d9c136d56d84ab2
matsume-hiro <matsume@techflyer.co.jp>
2022-08-21 01:58:22 +0900

    change setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true,
+  target: "static",
   head: {
     title: "LifeGame",
   },
