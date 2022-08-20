c24a9d61895acf86d8a4a3f0aa5cc000c862de30
matsume-hiro <matsume@techflyer.co.jp>
2022-08-21 03:09:03 +0900

    use npm i

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: "static",
-  head: {
-    title: "LifeGame",
-  },
   css: ["@/assets/css/main.scss", "@/assets/css/tailwind.css"],
   plugins: ["@/plugins/fontawesome.ts"],
   modules: ["@nuxtjs/tailwindcss"],
